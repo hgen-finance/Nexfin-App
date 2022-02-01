509e7cff7eedad3d617c1b724c0cdc5709a61fc8
AI-Pree <sexypistolcs@gmail.com>
2022-02-01 19:04:38 +1100

    refactor code

--- backend/db/index.js
+++ backend/db/index.js
@@ -2,9 +2,12 @@
 const mongoose = require("mongoose");
 
 //TODO: Add the URI of mongodb to env
-mongoose.connect("mongodb://mongo-nexfin:27017/data", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+mongoose
+  .connect("mongodb://mongo-nexfin:27017/data", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 module.exports = mongoose;
