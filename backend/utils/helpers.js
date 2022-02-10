9351cf63e9bc953a088e69c65efef91c810e64c8
AI-Pree <sexypistolcs@gmail.com>
2022-02-10 21:50:13 +1100

    check for values

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,6 +1,7 @@
 const BN = require('bn.js')
 
 const getCollateral = (gens, lamports, usd) => {
+    console.log(`gens: ${gens}, lamports :${lamports}, usd:${usd}`);
     return new BN(lamports).div(new BN("10000000")).mul(new BN(usd)).div(new BN(gens))
 }
 
