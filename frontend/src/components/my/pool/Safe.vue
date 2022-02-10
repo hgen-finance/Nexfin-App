a9d6e8468fce78798130ebbdeb53409aa786ceb5
AI-Pree <sexypistolcs@gmail.com>
2022-02-10 22:54:59 +1100

    change the hint from lend to borrow page

--- frontend/src/components/my/pool/Safe.vue
+++ frontend/src/components/my/pool/Safe.vue
@@ -7,18 +7,6 @@
         class="w-100 fs-8-S fs-25-XS fw-600 f-white-200 pb-2-S pb-10-XS ta-c-XS fd-r ai-c jc-sb"
       >
         Stake
-        <span class="info f-white-200">
-          <label
-            class="w-fix-s-12-S h-fix-s-12-S w-fix-s-42-XS h-fix-s-42-XS fd-r jc-c ai-c mcolor-100 rad-fix-2 fs-6-S fs-17-XS fw-600 ml-2-S ml-6-XS p-r"
-            >?</label
-          >
-          <span
-            class="popup w-fix-100min w-100max-S w-a-XS shadow-purple-100 p-2-S p-10-XS p-a l-0 t-100 fs-5-S fs-20-XS mcolor-500 rad-fix-3 fw-400 f-lh-13-S f-lh-43-XS z-15"
-          >
-            User are only able to withdraw their deposited SOL after their debt
-            is fully repaid.
-          </span>
-        </span>
       </div>
       <div class="w-100 fs-5-S fs-20-XS f-gray-500 pb-1-S pb-5-XS ta-c-XS">
         Your current pool share
