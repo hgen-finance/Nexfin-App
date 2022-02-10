0327b89359e5b7858b422c66db1ba42195827adc
AI-Pree <sexypistolcs@gmail.com>
2022-02-10 20:17:24 +1100

    remove decimal values to use bigNum

--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,9 +1,12 @@
-const BN = require('bn.js')
+const BN = require("bn.js");
 
 const getCollateral = (gens, lamports, usd) => {
-  return new BN(lamports).div(new BN("10000000")).mul(new BN(usd)).div(new BN(gens))
-}
+    return new BN(lamports)
+        .div(new BN("10000000"))
+        .mul(new BN(usd))
+        .div(new BN(parseInt(gens)));
+};
 
 module.exports = {
-  getCollateral,
-}
+    getCollateral,
+};
