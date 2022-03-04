0428240f532f9ab057dadd23dea8dae312390c8a
AI-Pree <sexypistolcs@gmail.com>
2022-03-05 02:45:13 +1100

    set env for setting the cluster url

--- frontend/src/plugins/web3.ts
+++ frontend/src/plugins/web3.ts
@@ -1,8 +1,29 @@
-import { Connection, clusterApiUrl } from "@solana/web3.js";
+import { Connection, clusterApiUrl, Cluster } from "@solana/web3.js";
 import { Plugin } from "@nuxt/types";
+import dotenv from 'dotenv';
+
+function chooseCluster(): Cluster | undefined {
+    dotenv.config();
+    if (!process.env.LIVE) return;
+    switch (process.env.CLUSTER) {
+        case 'devnet':
+        case 'testnet':
+        case 'mainnet-beta': {
+            return process.env.CLUSTER;
+        }
+    }
+    throw 'Unknown cluster "' + process.env.CLUSTER + '", check the .env file';
+}
+
+export const cluster = chooseCluster();
+
+export const url =
+    process.env.RPC_URL ||
+    (process.env.LIVE ? clusterApiUrl(cluster, false) : 'http://localhost:8899');
+
 
 const web3Plugin: Plugin = async (ctx, inject) => {
-    const web3 = new Connection("http://localhost:8899", "confirmed");
+    const web3 = new Connection(url, "confirmed");
     //   ctx.$web3 = web3
     inject("web3", web3);
     console.log(web3, "| web3 connection status");
